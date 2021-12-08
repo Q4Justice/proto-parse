87a9ce1df0a97c991786b1c5829d686587d5e250
Jennifer Nguyen <j.cnguyen3@gmail.com>
2021-12-08 15:22:33 -0800

    MVP-3170: Make ESLint run in all JavaScript files in the repo

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-const peg = require("pegjs");
-const fs = require("fs");
+const peg = require('pegjs');
+const fs = require('fs');
 
-fs.readFile('./proto.peg', function(err, data){
-	const parser = peg.generate(data.toString(), {output: "source", format: "commonjs"});
-	fs.writeFileSync('./parser.js', parser);
+fs.readFile('./proto.peg', function (err, data) {
+    const parser = peg.generate(data.toString(), { output: 'source', format: 'commonjs' });
+    fs.writeFileSync('./parser.js', parser);
 });
