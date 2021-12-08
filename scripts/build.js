664c93fd7ece3a653ac3d0ceff75fd3c9f3b0be4
Jennifer Nguyen <j.cnguyen3@gmail.com>
2021-12-08 15:23:05 -0800

    MVP-3170: Fix the input and output of the build

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,10 @@
 const peg = require('pegjs');
 const fs = require('fs');
 
-fs.readFile('./proto.peg', function (err, data) {
+const inputFilePath = './src/proto.peg';
+const outputFilePath = './src/parser.js';
+
+fs.readFile(inputFilePath, function (err, data) {
     const parser = peg.generate(data.toString(), { output: 'source', format: 'commonjs' });
-    fs.writeFileSync('./parser.js', parser);
+    fs.writeFileSync(outputFilePath, parser);
 });
